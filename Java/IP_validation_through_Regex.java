db0aca69f7d93eb5486ee7c6f7a299123bb55729
ak2732 <76532523+ak2732@users.noreply.github.com>
2022-10-06 22:46:31 +0530

    Revert "IP address validation using Regex."

--- Java/IP_validation_through_Regex.java
+++
@@ -1,8 +0,0 @@
-/* A regular expression such that it can be used to validate an IP address.
-Using the following definition of an IP address:
-  "IP address is a string in the form "A.B.C.D", where the value of A, B, C, and D may range from 0 to 255.
-  Leading zeros are allowed.
-  The length of A, B, C, or D can't be greater than 3." */
-class MyRegex{
-    public String pattern = "((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]))\\b";
-}
