b6de40e00b271c188f0593db39920ca5613ae47d
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2022-10-08 03:55:55 +0530

    updated fibonacci series
    array is replaced by loops to make the program user friendly and easy to understand .

--- Java/fibonacci_number.java
+++ Java/fibonacci_number.java
@@ -1,24 +1,30 @@
 import java.util.*;
 public class Fibonnaci {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         int FibLength;
         Scanner sc = new Scanner(System.in); // create object
-        System.out.print("Please enter length: ");
+        System.out.print("Please enter length upto which you have to find the series :");  //enter the length 
         FibLength = sc.nextInt();
 
-        int[] num = new int[FibLength];
+        int num1,num2,num3;
         // initialized first element to 0
-        num[0] = 0;
+        num1 = 0;
         // initialized second element to 1
-        num[1] = 1;
+        num2 = 1;
+        System.out.println("Fibonacci Series: ");
+        System.out.print( "  " + num1);
+        System.out.print( "  "+ num2);
 
         for (int i = 2; i < FibLength; i++) {
-            num[i] = num[i - 1] + num[i - 2];
-            System.out.println("Fibonacci Series: ");
+            num3=num1+num2;
+            System.out.print(" " + num3);
+            num1=num2;
+            num2=num3;
+            
         }
         for (int i = 0; i < FibLength; i++) {
-            System.out.print(num[i] + " ");
+            System.out.print(" ");
         }
     }
 
