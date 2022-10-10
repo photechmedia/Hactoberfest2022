d936b88c82959a989ec5ff5914fd027d20d8d85e
Mahesh Wattamwar <49906419+mahiwattamwar@users.noreply.github.com>
2022-10-10 22:54:52 +0530

    Add files via upload
    C++ Program to find the Missing Number

---
+++ C++/missing-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int findMissingNo (int arr[], int len){
+   int temp;  
+   temp  = ((len+1)*(len+2))/2;  
+   for (int i = 0; i<len; i++)    
+      temp -= arr[i];  
+   return temp;
+}
+int main() {
+   int n;   
+   cout<<"Enter the size of array: "; 
+   cin>>n;    int arr[n-1];  
+   cout<<"Enter array elements: ";   
+   for(int i=0; i<n; i++){    
+      cin>>arr[i];  
+   } 
+   int missingNo = findMissingNo(arr,5); 
+   cout<<"Missing Number is: "<<missingNo;
+   return 0;
+}
